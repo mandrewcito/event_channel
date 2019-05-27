44743abcc5dd16a7862167d5db4e821e11ca8a11
mandrewcito <anbaalo@gmail.com>
2019-05-28 00:09:46 +0200

    add multi-value test

--- test/event_channel_test.py
+++ test/event_channel_test.py
@@ -7,9 +7,15 @@ class TestStringMethods(unittest.TestCase):
     def setUp(self):
         self.channel = EventChannel()
         self.myvalue = None
+        self.myvalue_x = None
+        self.myvalue_y = None 
+        self.myvalue_z = None
 
     def tearDown(self):
         self.myvalue = None
+        self.myvalue_x = None
+        self.myvalue_y = None 
+        self.myvalue_z = None
 
     @classmethod
     def setUpClass(cls):
@@ -36,6 +42,11 @@ class TestStringMethods(unittest.TestCase):
     def aux_func(self, x):
         self.myvalue = x
 
+    def multi_aux_func(self, x, y, z):
+        self.myvalue_x = x
+        self.myvalue_y = y
+        self.myvalue_z = z
+
     def test_publish(self):
         evt = "myevent"
         func = self.aux_func
@@ -43,5 +54,17 @@ class TestStringMethods(unittest.TestCase):
         self.assertIn(func, self.channel.subscribers[evt])
         self.channel.publish(evt, 345)
         self.assertEqual(self.myvalue, 345)
+        self.channel.unsubscribe(evt, func)
+        self.assertNotIn(func, self.channel.subscribers[evt])
+
+    def test_publish_multi(self):
+        evt = "myevent"
+        func = self.multi_aux_func
+        self.channel.subscribe(evt, func)
+        self.assertIn(func, self.channel.subscribers[evt])
+        self.channel.publish(evt, 345, "asf", 333)
+        self.assertEqual(self.myvalue_x, 345)
+        self.assertEqual(self.myvalue_y, "asf")
+        self.assertEqual(self.myvalue_z, 333)
         self.channel.unsubscribe(evt, func)
         self.assertNotIn(func, self.channel.subscribers[evt])
