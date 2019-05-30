ae7ec0b1f9c23a36fb11422f4ea862623864e193
mandrewcito <anbaalo@gmail.com>
2019-05-30 22:11:53 +0200

    update post example

--- resources/examples/comparison_threaded.py
+++ resources/examples/comparison_threaded.py
@@ -4,6 +4,7 @@ from event_channel.threaded_event_channel import ThreadedEventChannel
 
 non_thread = EventChannel()
 threaded = ThreadedEventChannel()
+non_blocking_threaded = ThreadedEventChannel(blocking=False)
 
 non_thread.subscribe("myevent", time.sleep)
 non_thread.subscribe("myevent", time.sleep)
@@ -12,6 +13,7 @@ non_thread.publish("myevent", 3)
 end = time.time()
 print("non threaded function elapsed time {0}".format(end - start))
 #non threaded function elapsed time 6.0080871582
+
 threaded.subscribe("myevent", time.sleep)
 threaded.subscribe("myevent", time.sleep)
 start = time.time()
@@ -19,3 +21,11 @@ threaded.publish("myevent", 3)
 end = time.time()
 print("threaded function elapsed time {0}".format(end - start))
 # threaded function elapsed time 3.00581121445
+
+non_blocking_threaded.subscribe("myevent", time.sleep)
+non_blocking_threaded.subscribe("myevent", time.sleep)
+start = time.time()
+non_blocking_threaded.publish("myevent", 3)
+end = time.time()
+print("threaded function non blocking elapsed time {0}".format(end - start))
+# threaded function non blocking elapsed time 0.00333380699158
